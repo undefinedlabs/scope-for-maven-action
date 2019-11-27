22f11931acf50483f37d892ca5043a211528be97
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2019-11-27 08:49:20 +0100

    Added instrumentation

--- src/executor.ts
+++ src/executor.ts
@@ -1,5 +1,44 @@
-import * as core from '@actions/core';
+let tempDirectory = process.env['RUNNER_TEMP'] || '';
 
-export async function runTestsWithScope(apikey: string): Promise<void> {
-    core.debug("Received Scope APIKey: " + apikey)
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import * as tc from '@actions/tool-cache';
+import * as path from 'path';
+
+const IS_WINDOWS = process.platform === 'win32';
+
+if (!tempDirectory) {
+    let baseLocation;
+    if (IS_WINDOWS) {
+        // On windows use the USERPROFILE env variable
+        baseLocation = process.env['USERPROFILE'] || 'C:\\';
+    } else {
+        if (process.platform === 'darwin') {
+            baseLocation = '/Users';
+        } else {
+            baseLocation = '/home';
+        }
+    }
+    tempDirectory = path.join(baseLocation, 'actions', 'temp');
+}
+
+export async function instrument(agentVersion="0.2.2"): Promise<void> {
+    const workdir = process.cwd();
+    let scopeAgentPath = tc.find("scope-java-agent", agentVersion);
+
+    if(scopeAgentPath){
+        core.debug(`Scope Agent found in cache ${scopeAgentPath}`)
+    } else{
+        let destinationFolder: string = path.join(
+            tempDirectory,
+            'temp_' + Math.floor(Math.random() * 2000000000)
+        );
+        await io.mkdirP(destinationFolder);
+        await exec.exec("mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy -Dartifact=com.undefinedlabs.scope:scope-agent:"+agentVersion+":jar -Dtransitive=false -DoutputDirectory="+destinationFolder);
+
+        scopeAgentPath = await tc.cacheFile(path.join(destinationFolder, "scope-agent-"+agentVersion+".jar"), "scope-agent-"+agentVersion+".jar", "scope-java-agent", agentVersion);
+    }
+
+    await exec.exec("docker run -v "+workdir+":/home/project -e \"SCOPE_AGENT_PATH="+path.join(scopeAgentPath, "scope-agent-"+agentVersion+".jar")+"\" codescope/scope-instrumentation-for-maven")
 }
--- src/scope-for-maven.ts
+++ src/scope-for-maven.ts
@@ -1,11 +1,16 @@
 import * as core from '@actions/core'
 import * as executor from './executor'
+import * as exec from "@actions/exec";
 
 async function run() {
     try {
+        let scopeAgentVersion = core.getInput("agentVersion", {required: false}) || undefined;
         let apikey = core.getInput("apikey", {required: true})
 
-        await executor.runTestsWithScope(apikey)
+        console.log(scopeAgentVersion);
+
+        await executor.instrument(scopeAgentVersion);
+        exec.exec("cat pom.xml")
 
     } catch (error) {
         core.setFailed(error.message)
