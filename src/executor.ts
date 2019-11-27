e0f28a4bc572a7a08b6720a4bf226d8072fb87f9
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2019-11-27 08:49:20 +0100

    Added instrumentation

--- src/executor.ts
+++ src/executor.ts
@@ -1,5 +1,14 @@
-import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
-export async function runTestsWithScope(apikey: string): Promise<void> {
-    core.debug("Received Scope APIKey: " + apikey)
+export async function instrument(): Promise<void> {
+    const workdir = process.cwd();
+    const scopeFolder = ".scope";
+
+    await exec.exec("mkdir -p "+scopeFolder);
+    await exec.exec("rm -f "+scopeFolder+"/scope-agent*.jar");
+    await exec.exec("mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy -Dartifact=com.undefinedlabs.scope:scope-agent:LATEST:jar -Dtransitive=false -DoutputDirectory="+scopeFolder);
+    await exec.exec("sh -c \"mv "+scopeFolder+"/scope-agent*.jar "+scopeFolder+"/scope-agent.jar\"");
+
+    const scopeAgentPath = workdir+"/"+scopeFolder+"/scope-agent.jar";
+    await exec.exec("docker run -v "+workdir+":/home/project -e \"SCOPE_AGENT_PATH="+scopeAgentPath+"\" codescope/scope-instrumentation-for-maven")
 }
--- src/scope-for-maven.ts
+++ src/scope-for-maven.ts
@@ -1,11 +1,13 @@
 import * as core from '@actions/core'
 import * as executor from './executor'
+import * as exec from "@actions/exec";
 
 async function run() {
     try {
         let apikey = core.getInput("apikey", {required: true})
 
-        await executor.runTestsWithScope(apikey)
+        await executor.instrument();
+        exec.exec("cat pom.xml")
 
     } catch (error) {
         core.setFailed(error.message)
